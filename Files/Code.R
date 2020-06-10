bb6acd5328171304baabd35aa38caaf16cf34a7a
Sebastian Montenegro <66041836+sebmontenegro@users.noreply.github.com>
2020-06-10 10:49:38 -0500

    Add files via upload

--- Files/Code.R
+++ Files/Code.R
@@ -1,4 +1,4 @@
-setwd("~/Documents/PhD/UTD/Classes/6. Summer 2020/Ex1")
+setwd("~/Documents/PhD/UTD/Classes/6. Summer 2020/Ex1")#Adjust accordingly
 
 
 preload <- function(x){
@@ -10,10 +10,13 @@ preload <- function(x){
     require(x,character.only = TRUE)
   }
 }
-preload("readr")
-preload("ggplot2")
+preload("readr")#For CSV files loading
+preload("ggplot2")#For plots
+preload("gridExtra")
 
-data <- read_csv("Chicago_Police.csv")
+
+data <- read_csv("Chicago_Police.csv")#CPD Tweets
+data2 <- read_csv("CPD.csv")#Tweets about police brutality in Chicago
 
 desc <- function(x){
   #This function shows the median, mean, min, and max of a numeric variable
@@ -22,19 +25,52 @@ desc <- function(x){
   mean <- c("Mean", round(mean(x),2))
   min <- c("Min",min(x))
   max <- c("Max",max(x))
-  output <- rbind.data.frame(median,mean,min,max)
+  output <- data.frame(rbind(median,mean,min,max))
+  row.names(output) <- NULL
   colnames(output) <- NULL
   print(output)
 }
 
-desc(data$replies)
-mean(data$replies)
-max(data$replies)
+pdf(file = "Plot1.pdf", width = 7, height = 5)
+ggplot(data[data$replies<quantile(data$replies, .99),], aes(x=replies)) +
+  geom_histogram(binwidth=1, colour="black", fill="deepskyblue3",breaks=seq(0, 50, by = 1)) +
+  theme_light() + labs(title="Number of Replies to @Chicago_Police Tweets",x ="Number of Replies", y = "Count")+
+annotation_custom(tableGrob(desc(data$replies)), xmin=35, xmax=45)
+dev.off()
 
-ggplot(data, aes(x=username, y=replies)) + geom_boxplot() + 
-  guides(fill=FALSE) + coord_flip()
+
+pdf(file = "Plot2.pdf", width = 7, height = 5)
+ggplot(data[data$retweets<quantile(data$retweets, .99),], aes(x=retweets)) +
+  geom_histogram(binwidth=1, colour="black", fill="deepskyblue3",breaks=seq(0, 50, by = 1)) +
+  theme_light() + labs(title="Number of Retweets of @Chicago_Police Tweets",x ="Number of Retweets", y = "Count")+
+  annotation_custom(tableGrob(desc(data$retweets)), xmin=35, xmax=45)
+dev.off()
 
 
+pdf(file = "Plot3.pdf", width = 7, height = 5)
+ggplot(data[data$favorites<quantile(data$favorites, .99),], aes(x=favorites)) +
+  geom_histogram(binwidth=1, colour="black", fill="deepskyblue3",breaks=seq(0, 50, by = 1)) +
+  theme_light() + labs(title="Number of Favs of @Chicago_Police Tweets",x ="Number of Favorites", y = "Count")+
+  annotation_custom(tableGrob(desc(data$favorites)), xmin=35, xmax=45)
+dev.off()
 
-plot(data$replies)
 
+days <- data.frame(table(weekdays(as.Date(data2$date))))
+names(days) <- c("DOW","Freq")
+days$DOW <- as.character(days$DOW)
+days$DOW <- factor(days$DOW, levels = c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))
+
+pdf(file = "Plot4.pdf", width = 7, height = 5)
+ggplot(days, aes(x=DOW, y=Freq)) + geom_bar(colour="black", stat="identity",fill="deepskyblue3") + 
+  theme_light() + labs(title="Day of the Week for Tweets Containing \'PoliceBrutality\' in Chicago Area",x ="Day of the Week", y = "Count")
+dev.off()
+
+data2$time <- strptime(data2$date, "%Y-%m-%d %H:%M:%S")
+data2$time <- as.factor(format(data2$time, "%H"))
+time <- data.frame(table(data2$time))
+names(time) <- c("TOD","Freq")
+
+pdf(file = "Plot5.pdf", width = 7, height = 5)
+ggplot(time, aes(x=TOD, y=Freq)) + geom_bar(colour="black", stat="identity",fill="deepskyblue3") + 
+  theme_light() + labs(title="Hour of the Day for Tweets Containing \'PoliceBrutality\' in Chicago Area",x ="Time of the Deek", y = "Count")
+dev.off()
